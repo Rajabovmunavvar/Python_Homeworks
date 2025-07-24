b1e176a850b3dfaa25a444c53ffcfb8c0da1e953
Rajabovmunavvar <bittrade2003@gmail.com>
2025-07-25 01:48:20 +0500

    Create main.py

---
+++ lesson-11/homework/main.py
@@ -0,0 +1,32 @@
+# 1 ) Create your own virtual environment and install some python packages.
+
+# since  it must be done in terminal section , i am giving the codes for that in commented way !!!
+
+# python -m venv env
+# Set-ExecutionPolicy Unrestricted -Scope Process
+# .\env\Scripts\Activate
+# pip install pandas
+# pip install numpy
+
+
+# 2)Create custom modules.
+
+# Create math_operations.py module. Define add, subtract, multiply and divide functions in it. (All functions accept two arguments in this task)
+# Create string_utils.py module. Define reverse_string and count_vowels functions in it. (All functions accept one argument in this task)
+
+
+
+# 3) Create custom packages.
+# Create geometry package.
+#  geometry\
+#      __init__.py
+#      circle.py
+ 
+# Define calculate_area and calculate_circumference functions in circle.py. These functions accept one argument(radius).
+# Create file_operations package.
+#  file_operations\
+#      __init__.py
+#      file_reader.py
+#      file_writer.py
+ 
+# Define read_file function in file_reader.py. This function accepts one argument(file_path). Define write_file function in file_writer.py. This function accepts two arguments(file_path, content).
